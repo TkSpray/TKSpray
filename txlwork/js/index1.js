8edaa18d6593bb76c7ddeb98e997fdb4a8042b51
727393818 <727393818@qq.com>
2018-12-17 18:27:35 +0800

    .

--- txlwork/js/index1.js
+++ txlwork/js/index1.js
@@ -6,7 +6,7 @@ window.onload = function () {
     var unfinished1 = '';
     btn.innerHTML = '开始测评';
     p.className = 'text';
-    p.innerHTML = '欢迎您参加2018年辅导员工作满意度测评<br><br>您需要测评的辅导员是' + '<span>' + 'xxx' + '&nbsp&nbsp</span>' + '<br>还需要对' + '<span>' + 'xxx' + '&nbsp&nbsp</span>' + '继续评测' + '<br><br>每位辅导员有对应的9个测评问题<br>请根据实际情况选择对应选项<br>若中途退出，数据将不予以保存<br>因此请尽量一次填写完问卷，谢谢!';
+    p.innerHTML = '欢迎您参加2018年辅导员工作满意度测评<br><br>测评截止时间为<span class ="ppp">2018年12月24日晚12点</span><br>请及时填写问卷！<br><br>您需要测评的辅导员是' + '<span class="pp">' + '张学友' + '&nbsp&nbsp</span>' + '<br>还需要对&nbsp&nbsp' + '<span class = "pp">' + '张碧晨' + '&nbsp&nbsp</span>' + '继续评测' + '<br><br>每位辅导员有对应的9个测评问题<br>请根据实际情况选择对应选项<br>若中途退出，数据将不予以保存<br>因此请尽量一次填写完问卷，谢谢!';
 
     if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp = new XMLHttpRequest();
@@ -38,7 +38,7 @@ window.onload = function () {
                 }
                 btn.innerHTML = '开始评测';
                 p.className = 'text';
-                p.innerHTML = '欢迎您参加2018年辅导员工作满意度测评<br><br>您需要测评的辅导员是' + finished1 + unfinished1 + '<br>还需要对&nbsp&nbsp' + unfinished1 + '评测' + '<br><br>每位辅导员有对应的10个测评问题,请根据实际情况选择对应选项<br>若中途退出，数据将不予以保存<br>因此请尽量一次填写完问卷，谢谢!';
+                p.innerHTML = '欢迎您参加2018年辅导员工作满意度测评<br><br>测评截止时间为<span class ="ppp">2018年12月24日晚12点</span><br>请及时填写问卷！<br><br>您需要测评的辅导员是' + finished1 + unfinished1 + '<br>还需要对&nbsp&nbsp' + unfinished1 + '评测' + '<br><br>每位辅导员有对应的10个测评问题,请根据实际情况选择对应选项<br>若中途退出，数据将不予以保存<br>因此请尽量一次填写完问卷，谢谢!';
                 btn.onclick = function () {
                     window.location.href = "answer.html";
                 };
