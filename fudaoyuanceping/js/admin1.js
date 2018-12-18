9b25c510feffa09f274abad1a1ef0675a0c0a1b9
727393818 <727393818@qq.com>
2018-12-18 18:32:13 +0800

    .

---
+++ fudaoyuanceping/js/admin1.js
@@ -0,0 +1,70 @@
+var list1 = '';
+var list2 = '<p>未完成学生名单</p><br>';
+var list3 = '';
+var a = [1, 2, 3, 4];
+var div1 = document.getElementsByClassName('sec2');
+var div2 = document.getElementsByClassName('sec3');
+var div3 = document.getElementsByClassName('sec4');
+var que = ['Q1.作风正派，责任心强，工作有激情', 'Q2.工作能力强，有成效', 'Q3.关心同学，帮助解决思想上的困惑', 'Q4.引导同学成长成才，指导职业规划、就业考研和创新创业', 'Q5.关注同学学业，深入课堂，关心同学的学习进步和变化', 'Q6.深入寝室，与同学交心谈心，开展寝室文化建设和卫生安全教育活动', 'Q7.在各类评选、评定中公正无私，真实透明', 'Q8.关心关爱家庭经济困难学生', 'Q9.妥善协调同学关系，及时化解各种矛盾', 'Q10.总体评价'];
+var btn = document.getElementById('btn');
+var h1 = document.querySelector('h1');
+var nm = decodeURI(window.location.href.split('=')[1]);
+var btn1 = document.getElementById('btn1');
+
+h1.innerHTML = '辅导员' + nm + '工作满意度测评';
+
+window.onload = function () {
+    var Name = new FormData();
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else { // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var xml = JSON.parse(xmlhttp.response);
+            var data = xml.data;
+            var names = data.names;
+            var finished = data.finished;
+            var unfinished = data.unfinished;
+            var ques = data.questions;
+            var points = ques.ques_all;
+            var all = Number(finished) + Number(unfinished);
+            var per = finished / all;
+
+            if (xml.errorCode == 0 || xml.errorCode == 1 || xml.errorCode == 4 || xml.errorCode == 7) {
+                list1 = '<span>' + nm + '</span>' + '<span>总平均分:' + points + '</span>' + '<span>学生完成度:' + finished + '/' + all + '(' + toPercent(per) + ')' + '</span>' + '</div>';
+                div1[0].innerHTML = list1;
+
+                for (var a = 0; a < names.length; a++) {
+                    list2 += '<span>' + '&nbsp' + '&nbsp' + names[a] + '</span>';
+                }
+
+                for (var n = 0; n < que.length; n++) {
+                    list3 += '<div class="part"><p>' + que[n] + '</p>' + '<span>平均分:' + Object.values(ques)[n + 1] + '分</span></div>';
+                }
+
+                div2[0].innerHTML = list2;
+                div3[0].innerHTML = list3;
+            } else {
+                alert('系统错误');
+            }
+        }
+    };
+    Name.append('teacher_name', nm);
+    xmlhttp.open('post', 'http://120.79.199.124/evaluation/public/admin/teacher', true);
+    xmlhttp.withCredentials = true;
+    xmlhttp.send(Name);
+};
+btn.onclick = function () {
+    window.location.href = "http://120.79.199.124/evaluation/public/admin/download?teacher=" + nm;
+};
+btn1.onclick = function () {
+    window.location.href = "admin.html";
+};
+
+function toPercent(point) {
+    var str = Number(point * 100).toFixed(2);
+    str += "%";
+    return str;
+}
---
+++ fudaoyuanceping/js/answer.js
@@ -0,0 +1,189 @@
+var data = new FormData();
+var btn = document.getElementById("btn");
+var input = document.getElementsByName("ques");
+var n = 1;
+var p1 = document.getElementsByClassName("p1")[0];
+var p2 = document.getElementsByClassName("p1")[1];
+var que = document.getElementsByClassName("question")[0];
+var list = "";
+var wait = 1;
+var cho = false;
+var b = 0;
+p1.innerHTML = "您需要评议的辅导员有" + "<br>";
+p2.innerHTML = "您正在评议的辅导员是";
+
+window.onload = function () {
+  var xmlhttp1;
+  for (var a = 0; a < input.length; a++) {
+    input[a].setAttribute("tag", "0");
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp1 = new XMLHttpRequest();
+  } else {
+    // code for IE6, IE5
+    xmlhttp1 = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xmlhttp1.onreadystatechange = function () {
+    if (xmlhttp1.readyState == 4 && xmlhttp1.status == 200) {
+      var xml1 = JSON.parse(this.responseText);
+      var res = xml1.data;
+      var doing = res.unfinished[0];
+      var finished = res.finished;
+      var unfinished = res.unfinished;
+
+      if (xml1.errorCode == 0 || xml1.errorCode == 1 || xml1.errorCode == 4) {
+        data.append("teacher", doing);
+        if (unfinished.length == 1) {
+          p1.innerHTML = "";
+        } else {
+          for (var a = 0; a < unfinished.length; a++) {
+            list += '<span class = "pp1">' + "&nbsp" + "&nbsp" + unfinished[a] + "</em></span>";
+          }
+          p1.innerHTML = "您需要评议的辅导员有<br>" + list;
+        }
+        p2.innerHTML = '您正在评议的辅导员是   &nbsp<span class = "pp2">' + doing + "</span></em>";
+      } else if (xml1.errorCode == 5) {
+        alert("您已完成所有评测！");
+        window.location.href = "index.html";
+      } else if (xml1.errorCode == 2 || xml1.errorCode == 3) {
+        alert("未登录");
+        window.location.href = "index.html";
+      } else if (xml1.errorCode == 6) {
+        alert("密码错误!");
+        window.location.href = "index.html";
+      } else if (xml1.errorCode == 7) {
+        window.location.href = "admin.html";
+      } else if (xml.errorCode == 8) {
+        alert("非法操作，权限不够");
+        window.location.href = "index.html";
+      } else if (xml.errorCode == 10) {
+        alert("该学生不在评测范围内");
+        window.location.href = "index.html";
+      }
+    }
+  };
+  xmlhttp1.open("get", "http://120.79.199.124/evaluation/public/user/remains", true);
+  xmlhttp1.withCredentials = true;
+  xmlhttp1.send();
+
+  que.innerHTML = "1/10.作风正派，责任心强，工作有激情";
+};
+
+function request() {
+  var xmlhttp;
+
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    // code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var xml = JSON.parse(xmlhttp.response);
+      teacher = xml.teacher;
+      if (xml.errorCode == 0) {} else if (xml.errorCode == 1) {}
+    }
+  };
+  xmlhttp.open("post", "http://120.79.199.124/evaluation/public/user/store", true);
+  xmlhttp.withCredentials = true;
+  xmlhttp.send(data);
+  swal('您已完成该辅导员评测!', '', 'success').then(() => {
+    window.location.href = "index1.html";
+  });
+}
+
+btn.onclick = function () {
+  if (n <= 10) {
+    for (var i = 0; i < input.length; i++) {
+      if (input[i].checked == true) {
+        var value = input[i].value;
+        val = parseFloat(value);
+        data.append("ques_" + n, val);
+        n++;
+        cho = true;
+        switch (n) {
+          case 2:
+            que.innerHTML = "2/10.工作能力强，有成效";
+            break;
+          case 3:
+            que.innerHTML = "3/10.关心同学，帮助解决思想上的困惑";
+            break;
+          case 4:
+            que.innerHTML =
+              "4/10.引导同学成长成才，指导职业规划、就业考研和创新创业";
+            break;
+          case 5:
+            que.innerHTML =
+              "5/10.关注同学学业，深入课堂，关心同学的学习进步和变化";
+            break;
+          case 6:
+            que.innerHTML =
+              "6/10.深入寝室，与同学交心谈心，开展寝室文化建设和卫生安全教育活动";
+            break;
+          case 7:
+            que.innerHTML = "7/10.在各类评选、评定中公正无私，真实透明";
+            break;
+          case 8:
+            que.innerHTML = "8/10.关心关爱家庭经济困难学生";
+            break;
+          case 9:
+            que.innerHTML = "9/10.妥善协调同学关系，及时化解各种矛盾";
+            break;
+          case 10:
+            que.innerHTML = "10/10.总体评价";
+            break;
+          case 11:
+            console.log(data);
+            request();
+            break;
+        }
+        input[i].checked = false;
+        input[i].tag = 0;
+        time(this);
+      }
+    }
+    if (!cho) {
+      swal('请选择!', '', 'error');
+    }
+    cho = false;
+  }
+};
+
+function time(a) {
+  if (wait == 0) {
+    a.removeAttribute("disabled");
+    a.innerHTML = "下一题";
+    wait = 1;
+    b++;
+    console.log(b);
+    if (b == 18) {
+      a.innerHTML = "提交";
+    }
+  } else {
+    a.setAttribute("disabled", true);
+    a.innerHTML = "下一题(" + wait + ")";
+    wait--;
+    b++;
+    if (b == 17) {
+      a.innerHTML = "提交";
+    }
+    setTimeout(function () {
+      time(a);
+    }, 1000);
+  }
+}
+
+function check(radio) {
+  if (radio.tag == 1) {
+    radio.checked = false;
+    radio.tag = 0;
+  } else {
+    radio.checked = true;
+    radio.tag = 1;
+  }
+}
